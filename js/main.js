ad2fe05fb4033be2a7ff2d597120981d2ece1612
CarloCassini <c.cassini87@gmail.com>
2023-07-24 16:09:52 +0200

    end controllo email

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@ console.log("js ok");
 
 // dichiarazione delle variabili
 let userEmail;
-let accesso;
+let accesso = "ko";
 const myButtonPassword = document.getElementById("email-button");
+
+// ecco la lista di email:
+const email = [
+  "ciccio.pasticcio@gmail.com",
+  "paperino@gmail.com",
+  "prova",
+  "pluto@gmail.com",
+  "mammamia@gmail.com",
+];
 
 // al momento gestisco solo km e eta
 myButtonPassword.addEventListener("click", function () {
   userEmail = document.getElementById("user-email").value;
-
   console.log(userEmail);
 
-  document.getElementById("email-output").innerHTML = userEmail;
+  for (let i = 0; i < email.length && accesso == "ko"; i++) {
+    if (userEmail == email[i]) {
+      console.log("id dell'array " + i);
+      accesso = "ok";
+    }
+  }
+
+  //verifica di accesso
+  if (accesso == "ok") {
+    document.getElementById("email-output").innerHTML = userEmail;
+  } else {
+    document.getElementById("email-output").innerHTML =
+      "accesso non autorizzato";
+  }
 });
